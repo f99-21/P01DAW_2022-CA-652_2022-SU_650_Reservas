5cf09d6cf7ce2df11dc03e0cd5314925340daeec
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-02-27 17:15:18 -0600

    Agregar archivos de proyecto.

---
+++ P01_2022-CA-652_2022-SU_650/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace P01_2022_CA_652_2022_SU_650
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
