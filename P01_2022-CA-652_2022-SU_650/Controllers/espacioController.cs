7063a2cdd6250111d436e71b6527ecf85a7a646b
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-02-27 19:10:26 -0600

    registrar nuevos espacios

---
+++ P01_2022-CA-652_2022-SU_650/Controllers/espacioController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P01_2022_CA_652_2022_SU_650.Models;
+
+namespace P01_2022_CA_652_2022_SU_650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class espacioController : ControllerBase
+    {
+        private readonly reservaContext _espacioContext;
+
+        public espacioController(reservaContext espacioContext)
+        { 
+            _espacioContext  = espacioContext;
+        
+        }
+
+        [HttpGet]
+        [Route("Add")]
+        public ActionResult agregar ([FromBody]espacio espacios)
+        {
+            try
+            {
+                _espacioContext.espacios.Add(espacios);
+                _espacioContext.SaveChanges();
+                return Ok(espacios);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+            
+        
+    }
+}
